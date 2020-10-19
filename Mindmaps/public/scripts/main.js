8a0855ad5966590e47557f569d71ae2f9ffe14da
sugiyat <70905692+sugiyat@users.noreply.github.com>
2020-10-20 03:34:05 +0900

    Added the skeleton for the javascript

--- Mindmaps/public/scripts/main.js
+++ Mindmaps/public/scripts/main.js
@@ -10,21 +10,221 @@
 var rhit = rhit || {};
 
 /** globals */
-rhit.variableName = "";
+rhit.FB_COLLECTION_BUBBLES = "Bubbles";
+rhit.FB_COLLECTION_FOLDERS = "Folders";
+rhit.FB_COLLECTION_MINDMAPS = "Mindmaps";
+rhit.FB_COLLECTION_USERS = "Users";
+rhit.FB_KEY_DOCUMENT = "document";
+rhit.FB_KEY_BUBBLE_NAME = "bubbleName";
+rhit.FB_KEY_BUBBLE_PARENT_ID = "parentBubbleID";
+rhit.FB_KEY_MINDMAP_IDS = "mindmapIDs";
+rhit.FB_KEY_FOLDER_NAME = "folderName";
+rhit.FB_KEY_FOLDER_PARENT_ID = "parentFolderID";
+rhit.FB_KEY_DESCRIPTION = "description";
+rhit.FB_KEY_MINDMAP_NAME = "mindmapName";
+rhit.FB_KEY_ROOT_BUBBLE_ID = "rootBubbleID";
+rhit.FB_KEY_USER_ID = "foldersIDs";
+rhit.FB_KEY_PASSWORD = "password";
+rhit.FB_KEY_USER_NAME = "username";
+rhit.fbUserManager = null;
+rhit.fbBubbleManager = null;
+rhit.fbMindmapManager = null;
+rhit.fbFolderManager = null;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
+rhit.HomePageController = class {
 	constructor() {
 
 	}
 
-	methodName() {
+	updateView() {
 
 	}
+	createFolderCard(folder) {
+
+	}
+	createMindmapCard(mindMap) {
+
+	}
+}
+
+rhit.DocumentPageController = class {
+	constructor() {
+
+	}
+
+	updateView() {
+
+	}
+}
+
+rhit.MindmapPageController = class {
+	constructor() {
+
+	}
+
+	updateView() {
+
+	}
+	createBubbleView(bubble) {
+
+	}
+}
+
+rhit.FolderPageController = class {
+	constructor() {
+
+	}
+
+	updateView() {
+
+	}
+	createMindmapCard(mindMap) {
+
+	}
+}
+
+rhit.UserPageController = class {
+	constructor() {
+
+	}
+
+	updateView() {
+
+	}
+}
+
+rhit.TrashPageController = class {
+	constructor() {
+
+	}
+
+	updateView() {
+
+	}
+}
+
+rhit.User = class {
+	constructor(username, password, folderIDs) {
+		this.username = username;
+		this.password = password;
+		this.folderIDs = folderIDs;
+	}
+
+}
+
+rhit.Bubble = class {
+	constructor(document, name, parentID) {
+		this.document = document;
+		this.name = name;
+		this.parentID = parentID;
+	}
+
+}
+
+rhit.MindMap = class {
+	constructor(description, name, rootBubbleID) {
+		this.description = description;
+		this.name = name;
+		this.rootBubbleID = rootBubbleID;
+	}
+
+}
+
+rhit.Folder = class {
+	constructor(name, parentID, mindMapIDs) {
+		this.name = name;
+		this.parentID = parentID;
+		this.mindMapIDs = mindMapIDs;
+	}
+
+}
+
+rhit.FBUserManager = class {
+	constructor() {
+		
+	}
+	add(username, password) {
+
+	}
+	delete() {
+
+	}
+	update(username, password) {
+
+	}
+	beginListening(changeListener) {
+
+	}
+	stopListening() {
+
+	}
+
+}
+
+rhit.FBBubbleManager = class {
+	constructor() {
+		
+	}
+	add(name, parentID) {
+
+	}
+	delete() {
+
+	}
+	update(name, parentID) {
+
+	}
+	beginListening(changeListener) {
+
+	}
+	stopListening() {
+		
+	}
+
+}
+
+rhit.FBMindmapManager = class {
+	constructor() {
+		
+	}
+	add(name, description, rootBubbleID) {
+
+	}
+	delete() {
+
+	}
+	update(name, description) {
+
+	}
+	beginListening(changeListener) {
+
+	}
+	stopListening() {
+		
+	}
+
+}
+
+rhit.FBFolderManager = class {
+	constructor() {
+		
+	}
+	add(name, parentID) {
+
+	}
+	delete() {
+
+	}
+	update(name) {
+
+	}
+	beginListening(changeListener) {
+
+	}
+	stopListening() {
+		
+	}
+
 }
 
 /* Main */
