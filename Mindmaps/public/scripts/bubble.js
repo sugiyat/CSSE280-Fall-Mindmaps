e887a9b13aaacfffaa32f5bc7c42ebb03272cf9c
sugiyat <70905692+sugiyat@users.noreply.github.com>
2020-11-11 12:40:24 +0900

    Added Document and Edit Bubble

--- Mindmaps/public/scripts/bubble.js
+++ Mindmaps/public/scripts/bubble.js
@@ -1,7 +1,11 @@
 
+let bubbles = document.getElementsByClassName("bubble");
+let isDragging = false;
+let isLongPress = false;
+var pressTimer;
 
-createBubbles(document.getElementsByClassName("bubble"));
-dragBubble(document.getElementsByClassName("bubble"));
+createBubbles(bubbles);
+addBubbleGestures(bubbles);
 
 function createBubbles(bubbles) {
     for(i = 0; i < bubbles.length; i++) {
@@ -11,42 +15,70 @@ function createBubbles(bubbles) {
     }
 }
 
-function dragBubble(bubbles) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
+function addBubbleGestures(bubbles) {
+    var bubbleX = 0, bubbleY = 0, cursorX = 0, cursorY = 0;
     for(i = 0; i < bubbles.length; i++) {
         let bubble = bubbles[i];
         const maxWidth = document.getElementById("bubblePage").offsetWidth - bubble.offsetWidth;
         const maxHeight = document.getElementById("bubblePage").offsetHeight - bubble.offsetWidth;
-        bubble.onmousedown = (e1) => {
-            e1 = e1 || window.event;
-            e1.preventDefault();
-            
-            pos3 = e1.clientX;
-            pos4 = e1.clientY;
+
+        bubble.onmousedown = (downEvent) => {
+            downEvent = downEvent || window.event;
+            downEvent.preventDefault();
+
+            this.isDragging = false;
+            this.isLongPress = false;
+
+            cursorX = downEvent.clientX;
+            cursorY = downEvent.clientY;
 
             document.onmouseup = () => {
                 document.onmouseup = null;
                 document.onmousemove = null;
             };
 
-
-            document.onmousemove = (e2) => {
-                e2 = e2 || window.event;
-                e2.preventDefault();
-                
-                pos1 = pos3 - e2.clientX;
-                pos2 = pos4 - e2.clientY;
-                pos3 = e2.clientX;
-                pos4 = e2.clientY;
-                if ((bubble.offsetTop - pos2 >= 0) && (bubble.offsetLeft - pos1 >= 0) && (bubble.offsetLeft - pos1 <= maxWidth) && (bubble.offsetTop - pos2 <= maxHeight)) {
-                    bubble.style.top = (bubble.offsetTop - pos2) + "px";
-                    bubble.style.left = (bubble.offsetLeft - pos1) + "px";
+            document.onmousemove = (moveEvent) =>  {
+                moveEvent = moveEvent || window.event;
+                moveEvent.preventDefault();
+    
+                this.isDragging = true;
+    
+                bubbleX = cursorX - moveEvent.clientX;
+                bubbleY = cursorY - moveEvent.clientY;
+                cursorX = moveEvent.clientX;
+                cursorY = moveEvent.clientY;
+                if ((bubble.offsetTop - bubbleY >= 0) && (bubble.offsetLeft - bubbleX >= 0) && (bubble.offsetLeft - bubbleX <= maxWidth) && (bubble.offsetTop - bubbleY <= maxHeight)) {
+                    bubble.style.top = (bubble.offsetTop - bubbleY) + "px";
+                    bubble.style.left = (bubble.offsetLeft - bubbleX) + "px";
                 }
-
-                console.log("top:", bubble.style.top);
-                console.log("left:",bubble.style.left);
             };
+
+            pressTimer = window.setTimeout(function() {
+                if (!this.isDragging) {
+                    console.log("Long Press");
+                    this.isLongPress = true;
+
+                    //document.getElementById("editBubbleDialogue").modal(focus);
+                    $('#editBubbleDialogue').modal('show');
+                }
+                
+            },1500);
+            
+        };
+
+        bubble.onmouseup = (e) =>  {
+            clearTimeout(pressTimer);
+            
+            if (!this.isDragging && !this.isLongPress && e.button === 0) {
+                console.log('Press');
+
+                window.location.href = `/document.html`;
+                window.location.href = `/document.html?id=${document.id}`;
+            }
+
+            this.isLongPress = false;
+            this.isDragging = false;
         };
     }
 }
